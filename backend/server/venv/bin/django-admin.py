3696a26cb2014f8bc96bb75d9ec903ce817cded2
root <root@LAPTOP-JK3O9VQP.localdomain>
2021-04-21 16:24:00 +0700

    Endpoint models

---
+++ backend/server/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/imran273/ml_deployment_scenario_1/backend/server/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
